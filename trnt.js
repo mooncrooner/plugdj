baffbbfe969de08e56aa0c602fc25a3af379d1e7
Lauren <laurennutter@gmail.com>
2014-06-10 14:31:12 -0500

    Create trnt.js

---
+++ trnt.js
@@ -0,0 +1,47 @@
+{
+    "colors": {
+        "background": "",
+        "chat": {
+            "admin": "667A8E",
+            "ambassador": "6B8E26",
+            "bouncer": "66CCFF",
+            "cohost": "F55B36",
+            "residentdj": "55AA55",
+            "host": "F55B36",
+            "manager": "5E597F"
+        },
+        "footer": "",
+        "header": ""
+    },
+
+    "css": {
+        "font": [],
+        "import": [],
+        "rule": {}
+    },
+
+    "#plug-dj": { 
+        "background-image": "url('http://i.imgur.com/qap40GB.png') !important", 
+        "background-repeat": "no-repeat", 
+        "background-position": "center center"
+    }
+
+    "images": {
+        "background": "http://i.imgur.com/20aQbPW.png",
+        "booth": "",
+        "icons": {
+            "bouncer": "http://i.imgur.com/VYCLOEE.png",
+            "cohost": "http://i.imgur.com/DDfC8JB.png",
+            "residentdj": "http://i.imgur.com/PV3oLYL.png",
+            "host": "http://i.imgur.com/DDfC8JB.png",
+            "manager": "http://i.imgur.com/ah4bqht.png"
+        },
+        "playback": ""
+    },
+
+    "rules": {
+        "allowAutojoin": "true",
+        "allowAutorespond": "true",
+        "allowAutowoot": "true"
+    }
+}
